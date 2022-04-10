df04e725f040e7447e27b5d37dc7f9397dc673c2
Jetmarch <j3tmarch@gmail.com>
2022-04-10 15:12:12 +0300

    Add project files.

---
+++ Ezekiel/FileSort.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Text;
+
+namespace Ezekiel
+{
+    public class FileSort
+    {
+        Dictionary<string, string> extensionsAndDirectories;
+        string sortingDirectory;
+
+        public FileSort(string sortingDirectory)
+        {
+            extensionsAndDirectories = new Dictionary<string, string>();
+
+            extensionsAndDirectories["txt"] = "C:\\Users\\kranz\\Desktop\\Texts";
+
+            this.sortingDirectory = sortingDirectory;
+        }
+
+        public void Sort()
+        {
+            string[] files = Directory.GetFiles(sortingDirectory);
+
+            foreach(string file in files)
+            {
+                //Пропускаем точку в расширении файла
+                string fileExt = Path.GetExtension(file).Substring(1);
+                string pathForFileMove;
+                string fileName = Path.GetFileName(file);
+                
+                extensionsAndDirectories.TryGetValue(fileExt, out pathForFileMove);
+
+                if(pathForFileMove != null)
+                {
+                    /*Directory.Move(file, pathForFileMove);*/
+                    File.Move(file, $"{pathForFileMove}\\{fileName}");
+                }
+                else
+                {
+                    Console.WriteLine($"Неизвестно куда класть файлы с {fileExt} расширением");
+                }
+            }
+
+        }
+    }
+}
---
+++ Ezekiel/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Text;
+
+namespace Ezekiel
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.OutputEncoding = Encoding.UTF8;
+
+            FileSort fileSort = new FileSort("C:\\Users\\kranz\\Desktop");
+
+            fileSort.Sort();
+        }
+    }
+}
